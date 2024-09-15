f5f02fb995188a3db7ea325fdd0a09052a470a5f
sguruvad3 <shanguruvadoo@gmail.com>
2024-09-15 10:50:07 +0000

    update

---
+++ sampling_1/app.py
@@ -0,0 +1,266 @@
+import asyncio
+import boto3
+from botocore.exceptions import ClientError
+import datetime as datetime_object
+from datetime import time
+from datetime import datetime as datetime_parser
+from cassandra import ConsistencyLevel
+from cassandra.auth import PlainTextAuthProvider
+from cassandra.cluster import Cluster
+from cassandra.query import SimpleStatement
+from cassandra_sigv4.auth import SigV4AuthProvider
+import json
+import logging
+import os
+import pandas as pd
+from ssl import SSLContext, PROTOCOL_TLSv1_2 , CERT_REQUIRED
+import websockets
+
+def format_time(input_time:str) -> str:
+    '''
+    Formats timestamp for insertion to keyspace database
+    '''
+    timestamp_obj = pd.to_datetime(input_time, format='%Y-%m-%d %H:%M:%S.%f %z UTC')
+    
+    year = timestamp_obj.year
+    month = str(timestamp_obj.month).zfill(2)
+    day = str(timestamp_obj.day).zfill(2)
+    hours = str(timestamp_obj.hour).zfill(2)
+    minutes = str(timestamp_obj.minute).zfill(2)
+    seconds = str(timestamp_obj.second).zfill(2)
+    microseconds = str(timestamp_obj.microsecond)
+    milliseconds = microseconds[:-3]
+    timezone_info = timestamp_obj.tzinfo
+
+    #parse time zone
+    time_object = time(tzinfo=timezone_info)
+    timezone_offset = time_object.utcoffset().seconds//3600
+    timezone_offset = str(timezone_offset).zfill(4)
+    output_time = f'{year}-{month}-{day} {hours}:{minutes}:{seconds}.{milliseconds}+{timezone_offset }'
+    
+    return output_time
+
+class ingest_engine():
+    def __init__(self):
+        self.ais_socket_configuration_path = 'config/ais_socket_configuration.csv'
+        self.aws_user_credentials_path = 'config/ingest-engine-user-1_accessKeys.csv'
+        self.iam_role_credentials_path = 'config/cassandra_iam_role_credentials.csv'
+        self.keyspace_endpoint_path = 'config/keyspace_configuration.csv'
+        self.cassandra_security_certificate_path = 'config/sf-class2-root.crt'
+        
+        self.ais_web_socket_api_key = None
+        self.ais_web_socket_url = None
+
+        self.ais_socket_configuration_dataframe = None
+        self.aws_user_credentials_dataframe = None
+        self.iam_role_credentials_dataframe = None
+        self.keyspace_endpoint_dataframe = None
+
+        self.aws_user_access_key_id = None
+        self.aws_user_secret_access_key = None
+        self.iam_role_arn = None
+
+        self.aws_default_region = None
+
+        self.temporary_credentials_access_key_id = None
+        self.temporary_credentials_secret_key = None
+        self.temporary_credentials_session_token = None
+        self.iam_role_timeout = int(datetime_object.timedelta(hours=12).total_seconds()) #hours
+
+        #Parameters for keyspace connection and database instance
+        self.ssl_context = None
+        self.auth_provider = None
+        self.keyspace_endpoint = None
+        self.cluster_object = None
+        self.keyspace_port = 9142
+        self.keyspace_name = None
+        self.keyspace_table = None
+
+        #Parameters for keyspace table
+        self.id_column_name = 'id'
+        self.id_column_type = 'uuid'
+        self.latitude_column_name = 'lat'
+        self.latitude_column_type = 'float'
+        self.longitude_column_name = 'lon'
+        self.longitude_column_type = 'float'
+        self.mmsi_column_name = 'mmsi'
+        self.mmsi_column_type = 'varchar'
+        self.timestamp_column_name = 'time'
+        self.timestamp_column_type = 'timestamp'
+
+
+        #boto3 objects
+        self.aws_sts_client = None
+        self.aws_keyspaces_session = None
+
+        #timer objects
+        self.temporary_credentials_start_time_object = None
+
+        logging.basicConfig(filename="test.log", format="{asctime} - {levelname} - {message}", style="{", datefmt="%Y-%m-%d %H:%M", level=logging.INFO)
+
+        return
+
+    def load_ais_web_socket_configuration(self):
+        '''
+        Loads configuration parameters for connection to AIS data web socket
+        '''
+        self.ais_socket_configuration_dataframe = pd.read_csv(self.ais_socket_configuration_path)
+        self.ais_web_socket_api_key = self.ais_socket_configuration_dataframe['APIKey'][0]
+        self.ais_web_socket_url = self.ais_socket_configuration_dataframe['url'][0]
+        return
+
+    def load_aws_user_credentials(self):
+        '''
+        Loads credentials for AWS user
+        '''
+        self.aws_user_credentials_dataframe = pd.read_csv(self.aws_user_credentials_path)
+        self.aws_user_access_key_id = self.aws_user_credentials_dataframe['Access key ID'][0]
+        self.aws_user_secret_access_key = self.aws_user_credentials_dataframe['Secret access key'][0]
+        return
+
+    def load_cassandra_iam_role_credentials(self):
+        '''
+        Loads ARN for IAM role to interact with Cassandra
+        '''
+        self.iam_role_credentials_dataframe = pd.read_csv(self.iam_role_credentials_path)
+        self.iam_role_arn = self.iam_role_credentials_dataframe['ARN'][0]
+        return
+
+    def load_keyspace_configuration(self):
+        '''
+        Loads endpoint for keyspace
+        '''
+        self.keyspace_endpoint_dataframe = pd.read_csv(self.keyspace_endpoint_path)
+        self.aws_default_region = self.keyspace_endpoint_dataframe['region'][0]
+        self.keyspace_endpoint = self.keyspace_endpoint_dataframe['endpoint'][0]
+        self.keyspace_name = self.keyspace_endpoint_dataframe['keyspace'][0]
+        self.keyspace_table = self.keyspace_endpoint_dataframe['table'][0]
+        return
+
+    def get_keyspace_credentials(self):
+        '''
+        Returns temporary credentials to access AWS Keyspace (Cassandra database)
+        '''
+        self.load_aws_user_credentials()
+        self.aws_sts_client = boto3.client('sts', aws_access_key_id=self.aws_user_access_key_id, aws_secret_access_key=self.aws_user_secret_access_key, region_name=self.aws_default_region)
+        self.load_cassandra_iam_role_credentials()
+
+        try:
+            response = self.aws_sts_client.assume_role(RoleArn=self.iam_role_arn, RoleSessionName='ignored-by-weka-s3', DurationSeconds=self.iam_role_timeout)
+            self.temporary_credentials_access_key_id = response['Credentials']['AccessKeyId']
+            self.temporary_credentials_secret_key = response['Credentials']['SecretAccessKey']
+            self.temporary_credentials_session_token = response['Credentials']['SessionToken']
+        except ClientError as e:
+            logging.error(e)
+        
+        self.clear_aws_user_credentials()
+        self.close_aws_sts_boto3_session()
+        return
+
+    def setup_keyspace_connection(self):
+        '''
+        Loads connection parameters to interact with cassandra database
+        '''
+        self.load_keyspace_configuration()
+        self.ssl_context = SSLContext(PROTOCOL_TLSv1_2)
+        self.ssl_context.load_verify_locations(self.cassandra_security_certificate_path)
+        self.ssl_context.verify_mode = CERT_REQUIRED
+        self.aws_keyspaces_session = boto3.Session(aws_access_key_id=self.temporary_credentials_access_key_id, aws_secret_access_key=self.temporary_credentials_secret_key, aws_session_token=self.temporary_credentials_session_token, region_name=self.aws_default_region)
+        self.auth_provider = SigV4AuthProvider(self.aws_keyspaces_session)
+
+        self.cluster_object = Cluster([self.keyspace_endpoint], ssl_context=self.ssl_context, auth_provider=self.auth_provider, port=self.keyspace_port)
+        self.aws_keyspaces_session = self.cluster_object.connect()
+        return
+
+    def create_table(self):
+        '''
+        Creates table in keyspace
+        '''
+        sql_statement = f'CREATE TABLE IF NOT EXISTS {self.keyspace_name}.{self.keyspace_table} ({self.id_column_name} {self.id_column_type} PRIMARY KEY, {self.latitude_column_name} {self.latitude_column_type}, {self.longitude_column_name} {self.longitude_column_type}, {self.mmsi_column_name} {self.mmsi_column_type}, {self.timestamp_column_name} {self.timestamp_column_type});'
+        self.aws_keyspaces_session.execute(sql_statement)
+        return
+
+    def drop_table(self):
+        '''
+        Drops table in keyspace
+        '''
+        sql_statement = f'DROP TABLE IF EXISTS {self.keyspace_name}.{self.keyspace_table}'
+        self.aws_keyspaces_session.execute(sql_statement)
+        return
+
+    def select_all(self):
+        '''
+        select all records from keyspace cluster
+        '''
+        sql_statement = f'SELECT * FROM {self.keyspace_name}.{self.keyspace_table}'
+        response_records = self.aws_keyspaces_session.execute(sql_statement)
+        for record in response_records:
+            print(record.id, record.lat, record.lon, record.mmsi, record.time)
+        return
+
+    async def insert_records(self):
+        '''
+        Asynchronously insert records to keyspace
+        '''
+        self.load_ais_web_socket_configuration()
+        async with websockets.connect(self.ais_web_socket_url) as websocket:
+            subscribe_message = {"APIKey": self.ais_web_socket_api_key, "BoundingBoxes": [[[-11, 178], [30, 74]]]}
+
+            subscribe_message_json = json.dumps(subscribe_message)
+            await websocket.send(subscribe_message_json)
+
+            async for message_json in websocket:
+
+                message = json.loads(message_json)
+
+                latitude = message['MetaData']['latitude']
+                longitude = message['MetaData']['longitude']
+                timestamp = message['MetaData']['time_utc']
+                mmsi = message['MetaData']['MMSI']
+
+                #cast data types for insertion
+                latitude = float(latitude)
+                longitude = float(longitude)
+                mmsi = str(mmsi)
+                timestamp = format_time(timestamp)
+                
+                sql_statement = SimpleStatement(f"INSERT INTO {self.keyspace_name}.{self.keyspace_table} ({self.id_column_name}, {self.latitude_column_name}, {self.longitude_column_name}, {self.mmsi_column_name}, {self.timestamp_column_name}) VALUES(uuid(), {latitude}, {longitude}, '{mmsi}', '{timestamp}');", consistency_level=ConsistencyLevel.LOCAL_QUORUM)
+                try:
+                    self.aws_keyspaces_session.execute(sql_statement)
+                except:
+                    pass
+
+    def clear_aws_user_credentials(self):
+        '''
+        Clears AWS user credentials from memory
+        '''
+        self.aws_user_credentials_dataframe = None
+        self.aws_user_access_key_ID = None
+        self.aws_user_secret_access_key = None
+        return
+
+    def close_aws_sts_boto3_client(self):
+        '''
+        Clears self.aws_sts_client from memory
+        '''
+        self.aws_sts_client = None
+        return
+
+    def close_aws_sts_boto3_session(self):
+        '''
+        Clears self.aws_keyspaces_session from memory
+        '''
+        self.aws_keyspaces_session = None
+        return
+
+if __name__ == "__main__":
+    engine_object = ingest_engine()
+    engine_object.get_keyspace_credentials()
+    engine_object.setup_keyspace_connection()
+    engine_object.create_table()
+    asyncio.run(engine_object.insert_records())
+
+
+
+
+
